7c971163abfc02c0b09bb34f5c14ed3c17f70e2a
Quan Yin <quan.yin@invitae.com>
2021-01-09 14:21:13 -0800

    Created the EMR programming exercise Java project.

---
+++ src/com/invitae/emr/Main.java
@@ -0,0 +1,8 @@
+package com.invitae.emr;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is the start of your EMR programming exercise.");
+    }
+}
