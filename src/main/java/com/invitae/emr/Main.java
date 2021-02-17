25ecee99e4d650bf073a9fab6292ac66ffb65eb7
Quan Yin <quan.yin@invitae.com>
2021-02-17 14:02:26 -0800

    Add an example unit test using JUnit 5

--- src/main/java/com/invitae/emr/Main.java
+++ src/main/java/com/invitae/emr/Main.java
@@ -2,24 +2,16 @@ package com.invitae.emr;
 
 import com.invitae.emr.services.DataLoader;
 
-import java.io.IOException;
-
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is the start of your EMR programming exercise.");
 
         try {
-            /* load sample data */
-            var sampleData = DataLoader.loadSampleData();
-
-            /* execute your tests here */
-            assert sampleData.patients.size() == 10 : "Wrong number of patients.";
-        } catch (IOException ioException) {
+            DataLoader.loadSampleData();
+        } catch (Exception exception) {
             System.out.println("ERROR: failed to read from sample data files.");
-            ioException.printStackTrace();
-        } catch (AssertionError assertionError) {
-            System.out.println("Test failed: " + assertionError.getMessage());
+            exception.printStackTrace();
         }
     }
 }
