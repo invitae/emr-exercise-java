158b1813ee43cfda390397e12a265fe83f55f292
Quan Yin <quan.yin@invitae.com>
2021-03-28 15:21:04 -0700

    Simplify data models for the reporting exercise.

--- src/main/java/com/invitae/emr/models/LabReport.java
+++ src/main/java/com/invitae/emr/models/LabReport.java
@@ -6,22 +6,41 @@ import java.time.LocalDateTime;
  * The report produced by our lab, for a LabOrder.
  */
 public class LabReport {
+    /**
+     * Report ID
+     */
     public final String id;
+
+    /**
+     * The ID of the original LabOrder
+     */
     public final String orderId;
+
+    /**
+     * Date and time the report was produced
+     */
     public final LocalDateTime createdOn;
-    public final String type;
-    public final String subType;
+
+    /**
+     * List of genes and transcripts analyzed for this test
+     */
     public final GeneAnalyzed[] genesAnalyzed;
+
+    /**
+     * List of reported variants
+     */
     public final Variant[] variants;
+
+    /**
+     * List of clinical summaries
+     */
     public final String[] clinicalSummaries;
 
-    public LabReport(String id, String orderId, LocalDateTime createdOn, String type, String subType,
-                     GeneAnalyzed[] genesAnalyzed, Variant[] variants, String[] clinicalSummaries) {
+    public LabReport(String id, String orderId, LocalDateTime createdOn, GeneAnalyzed[] genesAnalyzed,
+                     Variant[] variants, String[] clinicalSummaries) {
         this.id = id;
         this.orderId = orderId;
         this.createdOn = createdOn;
-        this.type = type;
-        this.subType = subType;
         this.genesAnalyzed = genesAnalyzed;
         this.variants = variants;
         this.clinicalSummaries = clinicalSummaries;
--- src/main/java/com/invitae/emr/models/Result.java
+++ src/main/java/com/invitae/emr/models/Result.java
@@ -7,8 +7,19 @@ import java.util.List;
  * The test result we send to the ordering facility
  */
 public class Result {
+    /**
+     * The ID of the original LabOrder
+     */
     public final String orderId;
+
+    /**
+     * Date and time this result was created
+     */
     public final OffsetDateTime completionDateTime;
+
+    /**
+     * List of observations reported for this test
+     */
     public final List<ClinicalInfo> observations;
 
     public Result(String orderId, OffsetDateTime completionDateTime, List<ClinicalInfo> observations) {
